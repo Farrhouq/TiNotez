a0991bfeb2449b93f00ad1800225f8223bd85f38
Farouq <farouqimoro@gmail.com>
2023-02-22 18:55:14 +0000

    perfected the system overall

--- base_app/views.py
+++ base_app/views.py
@@ -4,20 +4,19 @@ import os
 
 # Create your views here.
 def home(request):
-    open_file = request.GET.get('file') # if request.GET.get('file') is not None else ''
+    open_file = request.GET.get('file')
 
-    directory = os.fsencode("C:/Users/Farouq/Desktop/")
+    directory = os.fsencode("C:/Users/Farouq/Documents/Notes/")
     file_list = []
     context = {}
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        if filename.endswith(".txt") or filename.endswith(".py"): 
+        if filename.endswith(".txt"): # or filename.endswith(".py") 
             file_list.append(filename)
 
     if open_file is not None:
-        with open(f'C:/Users/Farouq/Desktop/{open_file}', 'r') as file_opened:
+        with open(f'C:/Users/Farouq/Documents/Notes/{open_file}', 'r') as file_opened:
             notes = file_opened.readlines()
-
             context['notes'] = notes
             
     context['file_list'] = file_list
@@ -30,7 +29,7 @@ def save(request):
         notes = request.POST.get('notes')
 
         filename = request.POST.get('hidden-filename')
-        with open(f'C:/Users/Farouq/Desktop/{filename}', 'w') as file:
+        with open(f'C:/Users/Farouq/Documents/Notes/{filename}', 'w') as file:
             for note in notes:
                 file.write(note.rstrip('\n'))
     return redirect(f'/?file={filename}')
